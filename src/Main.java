b864316b7d562faacf7a3211534bbb8412996b22
ilonaesaulenko <ilonaesaulenko@yandex.ru>
2022-09-06 21:19:59 +0300

    miles-modernization

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
 
-        // Объявляете переменные для входных данных и
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной милли
-
-        // Рассчитываете количество бонусных милль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
+        public class Main {
+            public static void main(String[] args) {
+                BonusMilesService service = new BonusMilesService();
+                int price = 10_000;
+                int miles = service.calculate(price);
+                System.out.println(miles);
+            }
+        }
 
 
-        int ticketPrice = 5000;
-        int oneMile = 20;
-        int bonusMile = ticketPrice / oneMile;
-        int total = bonusMile;
-
-        System.out.println(total + "Миль");
-
-
-    }
-}
+    
